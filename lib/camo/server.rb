a74dbdfd1ab9a3f11eb69283ccfc85e3a6f66a81
Vyacheslav Alexeev <alexeev.corp@gmail.com>
2021-03-02 10:43:26 +0300

    Add digest validation & 2 formats

--- lib/camo/server.rb
+++ lib/camo/server.rb
@@ -1,22 +1,35 @@
 require 'rack/utils'
 require 'camo/client'
+require 'openssl'
 
 module Camo
   class Server
     include Rack::Utils
 
     def call(env)
-      return [400, {}, []] unless env['REQUEST_METHOD'] == 'GET' && env['PATH_INFO'] == '/'
+      return [400, {}, []] unless env['REQUEST_METHOD'] == 'GET'
       @query_string = env['QUERY_STRING']
-      return [400, {}, []] unless params.key?('url')
 
-      response = client.get(params.fetch('url'))
+      digest, encoded_url = env['PATH_INFO'][1..-1].split('/', 2)
+      @format = encoded_url ? :path : :query
 
+      destination_url =
+        if query_format?
+          params['url']
+        else
+          decode_hex(encoded_url)
+        end
+      return [400, {}, []] unless destination_url
+      return [401, {}, ['Invalid digest']] unless valid_digest?(digest, destination_url)
+
+      response = client.get(destination_url)
       headers = response[1]
       headers.delete 'transfer-encoding'
 
       [response[0], headers, [response[2]]]
     end
+
+    private
 
     def client
       @client ||= Client.new
@@ -26,8 +39,16 @@ module Camo
       @params ||= parse_query(@query_string)
     end
 
-    def query_parser
-      @query_parser ||= Rack::QueryParser.new(Hash)
+    def query_format?
+      @format == :query
+    end
+
+    def decode_hex(str)
+      [str].pack('H*')
+    end
+
+    def valid_digest?(digest, url)
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['CAMORB_KEY'], url) == digest
     end
   end
 end
--- spec/lib/camo/server_spec.rb
+++ spec/lib/camo/server_spec.rb
@@ -1,28 +1,110 @@
 require 'spec_helper'
+require 'openssl'
 
 describe Camo::Server do
-  def camo_url(url)
-    "/?url=#{url}"
+  def camo_url(url, format: :path)
+    raise ArgumentError, 'Format argument must be :path (default) or :query' unless [:path, :query].include? format
+    hexdigest = OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['CAMORB_KEY'], url)
+
+    return "/#{hexdigest}?url=#{url}" if format == :query
+    "/#{hexdigest}/#{encode_url(url)}"
   end
 
-  before { Timecop.freeze(Time.utc(1996, 9, 28)) }
-  after { Timecop.return }
+  def encode_url(url)
+    url.bytes.map { |byte| '%02x' % byte }.join
+  end
 
-  it 'returns the content of the page' do
-    mock_server('hello_world_server') do |uri|
-      get camo_url(uri)
+  before do
+    Timecop.freeze(Time.utc(1996, 9, 28))
+    @old_key = ENV['CAMORB_KEY']
+    ENV['CAMORB_KEY'] = 'somekey'
+  end
 
-      expect(last_response.status).to eq(200)
-      expect(last_response.body).to eq(<<~HTML.chomp)
-        <!doctype html>\n<html>\n  <head></head>\n  <body>\n    <h1>Hello World!</h1>\n  </body>\n</html>
-      HTML
-      expect(last_response.headers).to match({
-        'x-custom-header' => 'custom value',
-        'connection' => 'close',
-        'content-length' => '91',
-        'date' => 'Sat, 28 Sep 1996 00:00:00 GMT',
-        'server' => "WEBrick/1.4.2 (Ruby/2.6.6/2020-03-31)",
-      })
+  after do
+    Timecop.return
+    ENV['CAMORB_KEY'] = @old_key
+  end
+
+  context 'when format is url' do
+    it 'returns the content of the page' do
+      mock_server('hello_world_server') do |uri|
+        get camo_url(uri)
+
+        expect(last_response.status).to eq(200)
+        expect(last_response.body).to eq(<<~HTML.chomp)
+          <!doctype html>\n<html>\n  <head></head>\n  <body>\n    <h1>Hello World!</h1>\n  </body>\n</html>
+        HTML
+        expect(last_response.headers).to match({
+          'x-custom-header' => 'custom value',
+          'connection' => 'close',
+          'content-length' => '91',
+          'date' => 'Sat, 28 Sep 1996 00:00:00 GMT',
+          'server' => "WEBrick/1.4.2 (Ruby/2.6.6/2020-03-31)",
+        })
+      end
+    end
+
+    context 'when digest is not provided' do
+      it 'returns 401 with error message' do
+        get '/?url=https://localhost:3000'
+
+        expect(last_response.status).to eq(401)
+        expect(last_response.body).to eq('Invalid digest')
+      end
+    end
+
+    context 'when digest is invalid' do
+      it 'returns 401 with error message' do
+        get '/digest?url=https://localhost:3000'
+
+        expect(last_response.status).to eq(401)
+        expect(last_response.body).to eq('Invalid digest')
+      end
+    end
+  end
+
+  context 'when format is query' do
+    it 'returns the content of the page' do
+      mock_server('hello_world_server') do |uri|
+        get camo_url(uri, format: :query)
+
+        expect(last_response.status).to eq(200)
+        expect(last_response.body).to eq(<<~HTML.chomp)
+          <!doctype html>\n<html>\n  <head></head>\n  <body>\n    <h1>Hello World!</h1>\n  </body>\n</html>
+        HTML
+        expect(last_response.headers).to match({
+          'x-custom-header' => 'custom value',
+          'connection' => 'close',
+          'content-length' => '91',
+          'date' => 'Sat, 28 Sep 1996 00:00:00 GMT',
+          'server' => "WEBrick/1.4.2 (Ruby/2.6.6/2020-03-31)",
+        })
+      end
+    end
+
+    context 'when digest is not provided' do
+      it 'returns 400' do
+        get "/#{encode_url('https://localhost:3000')}"
+
+        expect(last_response.status).to eq(400)
+      end
+    end
+
+    context 'when digest is invalid' do
+      it 'returns 401 with error message' do
+        get "/digest/#{encode_url('https://localhost:3000')}"
+
+        expect(last_response.status).to eq(401)
+        expect(last_response.body).to eq('Invalid digest')
+      end
+    end
+  end
+
+  context 'when url is not provided' do
+    it 'returns 400' do
+      get "/digest"
+
+      expect(last_response.status).to eq(400)
     end
   end
 end
