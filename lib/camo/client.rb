b2590934e2eefa066a254d2b418925fc853f6aa7
Vyacheslav Alexeev <vyacheslav.alexeev@nordigy.ru>
2021-04-07 19:38:18 +0300

    Add request headers

--- lib/camo/client.rb
+++ lib/camo/client.rb
@@ -2,9 +2,26 @@ require 'faraday'
 
 module Camo
   class Client
-    def get(url)
-      response = Faraday.get(url)
+    include HeadersUtils
+    ALLOWED_TRANSFERRED_HEADERS = %w(Host Accept Accept-Encoding) # TODO check - _ and case sensitivity
+
+    def get(url, transferred_headers = {})
+      url = URI.parse(url)
+      headers = build_request_headers(transferred_headers, url: url)
+
+      response = Faraday.get(url, {}, headers)
       [response.status, response.headers, response.body]
+    end
+
+    private
+
+    def build_request_headers(headers, url:)
+      headers = headers
+        .select { |k,_| ALLOWED_TRANSFERRED_HEADERS.include?(k) }
+        .merge(default_request_headers)
+
+      headers['Host'] = url.host if String(headers['Host']).empty?
+      headers
     end
   end
 end
--- spec/lib/camo/client_spec.rb
+++ spec/lib/camo/client_spec.rb
@@ -9,7 +9,7 @@ describe Camo::Client do
 
     it 'returns the body and headers of the resource' do
       mock_server('hello_world_server') do |uri|
-        status, headers, body = subject.get(uri)
+        status, headers, body = client.get(uri)
         expect(body).to eq(<<~HTML.chomp)
           <!doctype html>\n<html>\n  <head></head>\n  <body>\n    <h1>Hello World!</h1>\n  </body>\n</html>
         HTML
@@ -22,6 +22,33 @@ describe Camo::Client do
           'server' => "WEBrick/1.4.2 (Ruby/2.6.6/2020-03-31)",
         })
       end
+    end
+  end
+
+  describe '#build_request_headers' do
+    let(:url) { URI.parse('https://google.com') }
+    let(:allowed_headers) do
+      {
+        'Accept' => 'image/*',
+        'Accept-Encoding' => '*',
+      }
+    end
+
+    it 'filters transferred headers' do
+      built_headers = client.send(:build_request_headers, allowed_headers.merge('X-Not-Allowed-Header' => 'value'), url: url)
+
+      expect(built_headers).to include(allowed_headers)
+      expect(built_headers).not_to include('X-Not-Allowed-Header' => 'value')
+    end
+
+    it 'adds host if it does not present' do
+      built_headers = client.send(:build_request_headers, allowed_headers, url: url)
+      expect(built_headers).to include('Host' => 'google.com')
+    end
+
+    it 'does not change host if it presents' do
+      built_headers = client.send(:build_request_headers, { 'Host' => 'yandex.ru' }, url: url)
+      expect(built_headers).to include('Host' => 'yandex.ru')
     end
   end
 end
